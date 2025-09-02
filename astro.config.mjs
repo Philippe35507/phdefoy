f52c51d464f8e329134414a94dda70e28dadedc1
Philippe35507 <phdefoy@gmail.com>
2025-09-02 21:56:03 +0100

    PageSpeed Requêtes de blocage de l’affichage

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 // @ts-check
-
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 export default defineConfig({
-	site: 'https://phdefoy.com',
-	integrations: [mdx(), sitemap()],
+  site: 'https://phdefoy.com',
+  integrations: [mdx(), sitemap()],
+  build: {
+    inlineStylesheets: 'always' // inline tout le CSS => plus de <link rel="stylesheet"> bloquants
+  }
 });
+
